859bbe0dbd186d8dc3b54431378ec976f73c0f16
wadee3g <wadee3-2005@outlook.sa>
2026-01-20 20:38:22 +0300

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,8 +1,7 @@
 void main(List<String> arguments) {
   Person person1 = Person(name: "Wadee3", age: 21);
   person1.printName();
-  Person person2 = Person(name: "Hassan", age: 20);
-  person2.printName();
+
 }
 
 class Person {
